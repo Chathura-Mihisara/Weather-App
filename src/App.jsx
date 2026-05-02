058c6df8b03af2e5823f862b117ab801e3049c89
Chathura Mihisara <chathuramihisara55@gmail.com>
2026-05-02 19:49:39 +0530

    Create Main UI

--- src/App.jsx
+++ src/App.jsx
@@ -17,8 +17,22 @@ function MainDetailsBox({item,value}){
 
 
 function App(){
- 
+ const [city, setCity] = useState('');
+
   return<>
+  <div className="App">
+    <h1>Weather app</h1>
+    <input type="text" placeholder='Enter city name' value={city} onChange={(e) => setCity(e.target.value)} />
+    <button >Get Weather</button>
+    
+    <div>
+      <MainDetailsBox item="City" value={city} />
+      <MainDetailsBox item="Temperature" value={'N/A'} />
+      <MainDetailsBox item="Humidity" value={'N/A'} />
+      <MainDetailsBox item="Description" value={'N/A'} />
+    </div>
+  </div>
   </>
 }
+
 export default App
