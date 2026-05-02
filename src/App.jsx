c2a43a50274ca93e68a92c80e76709da03e072ec
Chathura Mihisara <chathuramihisara55@gmail.com>
2026-05-02 19:51:57 +0530

    real data fetching

--- src/App.jsx
+++ src/App.jsx
@@ -18,18 +18,26 @@ function MainDetailsBox({item,value}){
 
 function App(){
  const [city, setCity] = useState('');
+ const [weatherData, setWeatherData] = useState(null);
+
+ const  fetchWeatherData = async () => {
+     let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=0abdbb13261c239afb493cd6e3685d54`)
+     let data = await response.json()
+     console.log(data)
+     setWeatherData(data);
+ }
 
   return<>
   <div className="App">
     <h1>Weather app</h1>
     <input type="text" placeholder='Enter city name' value={city} onChange={(e) => setCity(e.target.value)} />
-    <button >Get Weather</button>
+    <button onClick={fetchWeatherData}>Get Weather</button>
     
     <div>
       <MainDetailsBox item="City" value={city} />
-      <MainDetailsBox item="Temperature" value={'N/A'} />
-      <MainDetailsBox item="Humidity" value={'N/A'} />
-      <MainDetailsBox item="Description" value={'N/A'} />
+      <MainDetailsBox item="Temperature" value={weatherData ? weatherData.main.temp  : 'N/A'} />
+      <MainDetailsBox item="Humidity" value={ weatherData ? weatherData.main.humidity : 'N/A'} />
+      <MainDetailsBox item="Description" value={weatherData ? weatherData.weather[0].description : 'N/A'} />
     </div>
   </div>
   </>
