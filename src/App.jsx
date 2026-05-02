f5e88cc33e9a40044da564360c408bf333898f7b
Chathura Mihisara <chathuramihisara55@gmail.com>
2026-05-02 19:48:22 +0530

    Create MainDetailsBox function

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
 import { useState } from 'react'
+import './App.css'
+
+
+function MainDetailsBox({item,value}){
+
+  return(
+    
+    <div >
+        <div><span>{item}</span></div>
+        <div><span>{value}</span></div>
+    </div>
+   
+  );
+}
+
+
 
 function App(){
  
   return<>
-  
   </>
 }
 export default App
