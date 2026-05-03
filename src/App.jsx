9315b8bab2555cccddf6d31b57432c6acc827b0a
Chathura Mihisara <chathuramihisara55@gmail.com>
2026-05-03 17:18:50 +0530

    add loading UI

--- src/App.jsx
+++ src/App.jsx
@@ -19,28 +19,36 @@ const API_KEY = "0abdbb13261c239afb493cd6e3685d54";
 function App() {
   const [city, setCity] = useState('');
   const [weatherData, setWeatherData] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   const fetchWeatherData = async (city) => {
-  try {
-    const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`
-    );
+    setLoading(true);
+    
+    try {
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+      );
 
-    const data = await response.json();
+      const data = await response.json();
 
-    if (data.cod !== 200) {
-      throw new Error(data.message);
+      if (data.cod !== 200) {
+        throw new Error(data.message);
+        
+      }
+
+      console.log(data);
+      setWeatherData(data);
+      setCity('');
+      
+    } catch (error) {
+      console.error("Error:", error.message);
+      setWeatherData(weatherData);
+      alert("City not found or API error");
+
+    } finally {
+      setLoading(false);
     }
-
-    console.log(data);
-    setWeatherData(data);
-
-  } catch (error) {
-    console.error("Error:", error.message);
-    setWeatherData(weatherData);
-    alert("City not found or API error");
-  }
-};
+  };
 
   useEffect(() => {
     fetchWeatherData("Galle");
@@ -48,15 +56,21 @@ function App() {
 
   return <>
 
-  
+
     <div className="container">
+      {loading && (
+      <div className='overlay'>
+        <div className='loader'></div>
+        <p className='text'>Loading weather data...</p>
+      </div>
+    )}
 
       <div className='input-container'>
-        <input type="text" 
-        placeholder='Enter city name' 
-        value={city} 
-        onChange={(e) => setCity(e.target.value)} 
-        onKeyDown={(e) => e.key === 'Enter' && fetchWeatherData(city)} 
+        <input type="text"
+          placeholder='Enter city name'
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+          onKeyDown={(e) => e.key === 'Enter' && fetchWeatherData(city)}
         />
         <button onClick={() => fetchWeatherData(city)}>🔍︎</button>
       </div>
@@ -65,23 +79,24 @@ function App() {
       <div className='main-card'>
         <div className='modern-design'>
           <h5 >{weatherData ? weatherData.name : 'N/A'}</h5>
-          <h1>{weatherData ? (weatherData.main?.temp - 273.15).toFixed(0) + "°C" : 'N/A'}</h1>
+          <h1>{weatherData ? weatherData.main?.temp.toFixed(0) + "°C" : 'N/A'}</h1>
           <h6>{weatherData ? weatherData.weather[0]?.description : 'N/A'}</h6>
         </div>
         <div className='image'>
-        <img src="src/assets/pngtree-flat-sun-cloud-icon-free-vector-png-image_6582790.png" alt="Weather Icon" />
+          <img src="src/assets/pngtree-flat-sun-cloud-icon-free-vector-png-image_6582790.png" alt="Weather Icon" />
         </div>
+
+
+        
       </div >
 
-      
+
       <div className='main-details-container'>
         <MainDetailsBox item="Humidity" value={weatherData ? weatherData?.main?.humidity + "%" : 'N/A'} />
         <MainDetailsBox item="Wind Speed" value={weatherData ? (weatherData?.wind?.speed * 3.6).toFixed(1) + " km/h" : 'N/A'} />
       </div>
+
     </div>
-
-
-
   </>
 }
 
